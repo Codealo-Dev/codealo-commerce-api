1b34a2a41da853e3f32523cc3998da3229fab4e2
Johnnie Tirado <tiradoyamin@gmail.com>
2021-12-26 13:28:26 -0500

    Adding order information

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,4 +5,20 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        let entities;
+        const {user} = ctx.state;
+        
+        const {query} = ctx.query;
+        query.customer = user.id;
+
+        if (ctx.query._q) {
+            entities = await strapi.services.restaurant.search(ctx.query);
+        } else {
+            entities = await strapi.services.restaurant.find(ctx.query);
+        }
+
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.restaurant }));
+    }
+};
