3f81f63136826edfb61799d2c951c929cf81d583
Johnnie Tirado <tiradoyamin@gmail.com>
2021-12-27 07:13:17 -0500

    Fixing package json

--- api/cart/models/cart.js
+++ api/cart/models/cart.js
@@ -5,7 +5,8 @@
  * to customize this model
  */
 
-import { v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
+
 
 module.exports = {
     lifecycles: {
--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -9,16 +9,16 @@ module.exports = {
     async find(ctx) {
         let entities;
         const {user} = ctx.state;
-        
         const {query} = ctx.query;
+        
         query.customer = user.id;
 
         if (ctx.query._q) {
-            entities = await strapi.services.restaurant.search(ctx.query);
+            entities = await strapi.services.order.search(ctx.query);
         } else {
-            entities = await strapi.services.restaurant.find(ctx.query);
+            entities = await strapi.services.order.find(ctx.query);
         }
 
-        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.restaurant }));
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.order }));
     }
 };
