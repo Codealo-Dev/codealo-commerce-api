14574fd020a6c3080abba912652bec668fb3a153
Johnnie Tirado <tiradoyamin@gmail.com>
2021-12-27 09:07:55 -0500

    Adding data public

---
+++ api/common/controllers/common.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { sanitizeEntity } = require("strapi-utils/lib");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    async health(ctx) {
+        return { 'healthy': true };
+    }
+};
