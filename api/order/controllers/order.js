cdb4812459a277f0c4d69b421a413e572b90b849
Johnnie Tirado <tiradoyamin@gmail.com>
2021-12-27 07:25:03 -0500

    Fixing order to work correctly

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { sanitizeEntity } = require("strapi-utils/lib");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -8,9 +10,9 @@
 module.exports = {
     async find(ctx) {
         let entities;
-        const {user} = ctx.state;
-        const {query} = ctx.query;
-        
+        const { user } = ctx.state;
+        const { query } = ctx.query;
+
         query.customer = user.id;
 
         if (ctx.query._q) {
@@ -20,5 +22,36 @@ module.exports = {
         }
 
         return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.order }));
+    },
+    /**
+     * Create a record.
+     *
+     * @return {Object}
+     */
+
+    async create(ctx) {
+        const { user } = ctx.state;
+        const { body } = ctx.request;
+        body.customer = user;
+
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.services.order.create(data, { files });
+        } else {
+            entity = await strapi.services.order.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.order });
+    },
+    async cancel(ctx) {
+        const { user } = ctx.state;
+        const { id } = ctx.params;
+
+        let entity = await strapi.services.order.findOne(id);
+        ctx.assert(entity, 404, 'Order not found');
+        ctx.assert(entity.customer.id === user.id, 403, 'You cannot perform this action');
+
+        let entity = await strapi.services.order.update({ id: entity.id }, { 'cancelled': true });
+        return sanitizeEntity(entity, { model: strapi.models.order });
     }
 };
