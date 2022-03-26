cfb10df94e456278f7703597089be20acd6fed4c
Johnnie Tirado <tiradoyamin@gmail.com>
2022-03-26 08:20:00 -0400

    Adding verified buyer to reviews

--- api/reviews/controllers/reviews.js
+++ api/reviews/controllers/reviews.js
@@ -5,4 +5,36 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    async create(ctx) {
+        const { user } = ctx.state;
+        const data = ctx.request.body;
+        let entity;
+        let hasReviewed = await strapi.services.reviews.findOne({
+            'customer.id': user.id,
+            'product.id': data.product
+        });
+
+        if (hasReviewed) {
+            ctx.response.statusCode = 404;
+            return ctx.response.send({
+                error: true,
+                message: "User has reviewed product",
+                review: sanitizeEntity(hasReviewed, { model: strapi.models.reviews })
+            });
+        }
+
+        let orders = await strapi.services.order.find({ 'customer': user.id }, ['products']);
+        console.log(orders[0].products, data);
+        let hasPurchased = orders.some((o) => o.products.some(p => p.product.id == data.product));
+        data.verifiedBuyer = Boolean(hasPurchased);
+
+        data.customer = user.id;
+
+        entity = await strapi.services.reviews.create(data);
+        return sanitizeEntity(entity, { model: strapi.models.reviews });
+    }
+
+};
